05f81f9c610bb6407776252f22b545d05abf5209
Francisco J Vite <jeshuavhen@hotmail.com>
2023-09-16 23:18:22 -0600

    se resuelve problema de imagen bienvenida

--- webPizza.js
+++ webPizza.js
@@ -1,37 +1,16 @@
-        // Función para comprobar el tamaño de la pantalla y agregar la imagen si es mayor o igual a 1000px
-        function agregarImagen() {
-            // Obtener el ancho de la ventana
-            var anchoVentana = window.innerWidth;
+         // Función para cambiar la fuente de la imagen según el tamaño de la pantalla
+         function actualizarImagen() {
+            // Obtener el elemento de la imagen
+            var imagen = document.getElementById('pizzaImg');
 
-            // Verificar si el ancho de la ventana es mayor o igual a 1000px
-            if (anchoVentana >= 990) {
-                // Verificar si ya se ha agregado la imagen
-                var contenedor = document.getElementById('bienvenidaPizza');
-                var imagenExistente = contenedor.querySelector('img');
-
-                // Si no se ha agregado la imagen, la agregamos
-                if (!imagenExistente) {
-                    // Crear un elemento <img>
-                    var imagen = document.createElement('img');
-
-                    // Establecer los atributos de la imagen
-                    imagen.src = 'https://lh3.googleusercontent.com/pw/AIL4fc-ptpzqjT2xWyjk7QFmDdeHdr13iXuC9r2xZu_qnHc1MQ3Bg0tey9STYBdjxtVvdN9D8UTAZmjbyCoiPKy6neU9bgiL55idiYLP_gvI33803ro6dJhKROU8dnTVYe1WyiVfcT5o2x2kcWbl4_GWZ62enQ=w1103-h552-s-no?authuser=0';
-                    imagen.alt = 'Pizza deliciosa';
-
-                    // Agregar la imagen al contenedor
-                    contenedor.appendChild(imagen);
-                }
-            } else {
-                // Si el ancho de la ventana es menor a 1000px, eliminar la imagen si existe
-                var contenedor = document.getElementById('bienvenidaPizza');
-                var imagenExistente = contenedor.querySelector('img');
-
-                if (imagenExistente) {
-                    contenedor.removeChild(imagenExistente);
-                }
-            }
+            // Verificar el ancho de la pantalla y cambiar la fuente de la imagen
+            if (window.innerWidth >= 990) {
+                imagen.src = 'https://lh3.googleusercontent.com/pw/AIL4fc_AGi0Rokroyg1wlkX1fL0MPCDe2zFW7xYz-i6b1tBZfZeEl3L8TE3mmgvdFXOz382OJVcy6tpwcYOG4tNhbI9YnrIVRkFLVd2iGWdWcAd6BAp4TsLYLejqDx3OPB40cPy2h9KyQN9elath8ca18NXsEg=w1000-h500-s-no?authuser=0';
+            } else if (window.innerWidth < 990) {
+                imagen.src = 'https://lh3.googleusercontent.com/pw/AIL4fc_r7gSjDF45mNQFZNizWahoFbX3NffOoR6xXF-Aj8LtvP2h7B0Sn7rd76DGgcWxpwZMR5uCwgVwzC00wQ9FwlbGBZ3pLaQTYe5cUfSUSRjySElXxPew1q7y3ikwzhlWJeIM_XmdbHUMyEYtfjMAmI70Cg=w460-h919-s-no?authuser=0';
+            }  
         }
 
-        // Ejecutar la función al cargar la página y al cambiar el tamaño de la ventana
-        window.addEventListener('load', agregarImagen);
-        window.addEventListener('resize', agregarImagen);
+        // Llama a la función al cargar la página y cuando se redimensiona la ventana
+        window.addEventListener('load', actualizarImagen);
+        window.addEventListener('resize', actualizarImagen);
