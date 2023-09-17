b23dfcc679472364411aae80bc64919d3bb46623
Francisco J Vite <jeshuavhen@hotmail.com>
2023-09-17 17:00:25 -0600

    Optimizacion de css media query

--- webPizza.js
+++ webPizza.js
@@ -4,9 +4,9 @@
             var imagen = document.getElementById('pizzaImg');
 
             // Verificar el ancho de la pantalla y cambiar la fuente de la imagen
-            if (window.innerWidth >= 380) {
+            if (window.innerWidth >= 800) {
                 imagen.src = 'https://lh3.googleusercontent.com/pw/AIL4fc_AGi0Rokroyg1wlkX1fL0MPCDe2zFW7xYz-i6b1tBZfZeEl3L8TE3mmgvdFXOz382OJVcy6tpwcYOG4tNhbI9YnrIVRkFLVd2iGWdWcAd6BAp4TsLYLejqDx3OPB40cPy2h9KyQN9elath8ca18NXsEg=w1000-h500-s-no?authuser=0';
-            } else if (window.innerWidth < 380) {
+            } else if (window.innerWidth < 800) {
                 imagen.src = 'https://lh3.googleusercontent.com/pw/AIL4fc_r7gSjDF45mNQFZNizWahoFbX3NffOoR6xXF-Aj8LtvP2h7B0Sn7rd76DGgcWxpwZMR5uCwgVwzC00wQ9FwlbGBZ3pLaQTYe5cUfSUSRjySElXxPew1q7y3ikwzhlWJeIM_XmdbHUMyEYtfjMAmI70Cg=w460-h919-s-no?authuser=0';
             }  
         }
